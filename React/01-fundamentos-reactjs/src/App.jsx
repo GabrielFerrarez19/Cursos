3f3495a555af9e8b58b1f3d98ff269195ebab7a8
GabrielFerrarez19 <gabrielferrarez190206@gmail.com>
2025-07-02 08:27:01 -0300

    Aula 201

--- React/01-fundamentos-reactjs/src/App.jsx
+++ React/01-fundamentos-reactjs/src/App.jsx
@@ -1,5 +1,26 @@
-function App() {
-  return <h1>Hello, world!!</h1>;
+import { Header } from "./components/Header";
+import {Post}  from "./Post";
+import './global.css';
+
+export function App() {
+  return(
+
+  <div>
+
+      <Header />
+      <Post 
+        author="Gabriel Ferrarez" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, cum fuga veritatis saepe aut ullam nemo ratione tempore amet itaque, consequuntur quo! Fugiat soluta ad dolorem dicta qui eveniet quisquam!" 
+        />
+        <Post 
+        author="gabriel Cristiano" 
+        content="Um novo post foi adicionado ao blog. Confira!" 
+        />
+
+  </div> 
+  
+) 
+  ;
 }
 
-export default App;
+
