5ae0e38f939fc14416ccab72f3c54c7674e72e54
GabrielFerrarez19 <gabrieferrarez190206@gmail.com>
2025-10-10 17:54:13 -0300

    modulo2

--- Go/rocketseat/Modulo02/main.go
+++ Go/rocketseat/Modulo02/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+func main() {
+	err := foo()
+	fmt.Println(err)
+	fmt.Println(errors.Is(err, ErrQualquer))
+	fmt.Println(errors.Is(err, ErrQualquer2))
+}
+
+var (
+	ErrQualquer  = errors.New("error")
+	ErrQualquer2 = errors.New("error 2")
+)
+
+func a() error { return ErrQualquer }
+func b() error { return ErrQualquer2 }
+
+func foo() error {
+	var errorResult error
+	if err := a(); err != nil {
+		errorResult = errors.Join(errorResult, err)
+	}
+
+	if err := b(); err != nil {
+		errorResult = errors.Join(errorResult, err)
+	}
+
+	return errorResult
 }
